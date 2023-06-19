4adcbac3b3d929bbe1fc3d9cf60b21766900e8e2
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2023-06-19 06:12:24 +0200

    finish appjs component

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Launches from "../../Pages/Launches";
+import Header from "../Header";
+
+const App = () => {
+    return(
+        <React.Fragment>
+            <Header />
+            <Launches />
+        </React.Fragment>
+    )
+}
+
+export default App;
