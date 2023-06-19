37b358940df9558acb4effc9eacd16e4c661fde5
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2023-06-19 05:20:03 +0200

    create activity indicator / loader component

---
+++ src/Components/ActivityIndicator/ActivityIndicator.jsx
@@ -0,0 +1,13 @@
+import "./ActivityIndicator.scss";
+
+const ActivityIndicator = () => {
+    return(
+        <div className="activity__indicator">
+            <span className="inner-indicator-1"></span>
+            <span className="inner-indicator-2"></span>
+            <span className="inner-indicator-3"></span>
+        </div>
+    )
+}
+
+export default ActivityIndicator;
